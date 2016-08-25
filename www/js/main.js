27492c5399134cd6117e304817b8660ed0ba5106
Alex Karo <alex@karo-dev.ru>
2016-08-25 19:48:45 +0300

    Add navigation and sweet scroll

--- www/js/main.js
+++ www/js/main.js
@@ -4,4 +4,20 @@ $(function () {
        dots: true,
        dotsClass: 'reviews__dots'
    });
+
+    $('.js-scroll-to').on('click', function(e) {
+        e.preventDefault();
+        $(location.hash).scrollToBlock();
+    });
+
+    $.fn.scrollToBlock = function() {
+        var target = $(this),
+            headerHeight = $('.pseudo_header').height();
+        if ( target.length ) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+        return this;
+    };
 });
